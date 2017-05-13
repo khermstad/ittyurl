66ac36e5f07f85879e22998f78651c7f4080a99d
khermstad <krishermstad@gmail.com>
2017-05-13 00:53:41 -0400

    added MYSQL database connection

--- app.js
+++ app.js
@@ -14,6 +14,38 @@ app.use(express.static('public'))
 
 app.set('port', (process.env.PORT || 5001))
 
+const db_config = {
+  host: '107.180.44.152',
+  user: 'jkhermstad',
+  password: 'jkhermstad',
+  database: 'jkh_ittyurl'
+}
+
+var connection
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
+
+
 app.listen(app.get('port'), function(){
   console.log('tiny-url clone running on port', app.get('port'))
 })
@@ -27,5 +59,6 @@ app.get('/tiny/:tagId', function(req, res){
 })
 
 app.post('/getURL', (req, res) =>{
+  console.log(req.body.fullURL)
   res.redirect("http://www.google.com")
 })
